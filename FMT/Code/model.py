ba18a84d0d48127abaeca6168e2c098dfd08438d
bcm628 <bcmyers628@gmail.com>
2020-08-20 11:28:05 +0100

    updated acoustic extractor

--- FMT/Code/model.py
+++ FMT/Code/model.py
@@ -1,18 +1,67 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 from consts import global_consts as gc
 from transformer import Models
 
-#TODO: I think I can just remove these linear layers and put in my DANN output
-#TODO: is that all that the initial embedding layer is?
+#TODO: pass visual and acoustic input into Embedding Layer then input that output into Net()
+
+class AcousticEmbedding(nn.Module):
+    """Feedforward DNN feature extractor"""
+    def __init__(self, input_dims, output_dims):
+        super(AcousticEmbedding, self).__init__()
+        # self.batch_size = batch_size
+        # self.num_layers = num_layers
+        self.input_dims = input_dims
+        self.output_dims = output_dims
+
+        self.fc1 = nn.Linear(input_dims, 128)
+        self.fc2 = nn.Linear(128, 256)
+        #self.fc4 = nn.Linear(54, 64)
+        self.fc3 = nn.Linear(256, output_dims)
+        self.dropout = nn.Dropout()
+
+    def forward(self, x):
+        x = F.relu(self.dropout(self.fc1(x)))
+        #x = self.dropout(x)
+        x = F.relu(self.dropout(self.fc2(x)))
+        #x = F.relu(self.fc4(x))
+        x = self.fc3(x)
+
+        return x
+
+class VisualEmbedding(nn.Module):
+    """Feedforward DNN feature extractor"""
+    def __init__(self, input_dims, output_dims):
+        super(VisualEmbedding, self).__init__()
+        # self.batch_size = batch_size
+        # self.num_layers = num_layers
+        self.input_dims = input_dims
+        self.output_dims = output_dims
+
+        self.fc1 = nn.Linear(input_dims, 128)
+        self.fc2 = nn.Linear(128, 256)
+        #self.fc4 = nn.Linear(54, 64)
+        self.fc3 = nn.Linear(256, output_dims)
+        self.dropout = nn.Dropout()
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        #x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        #x = F.relu(self.fc4(x))
+        x = self.fc3(x)
+
+        return x
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         conf = gc.config
         proj_dim_a = conf['proj_dim_a']
         proj_dim_v = conf['proj_dim_v']
-        self.proj_a = nn.Linear(gc.dim_a, proj_dim_a)
-        self.proj_v = nn.Linear(gc.dim_v, proj_dim_v)
+        #self.proj_a = nn.Linear(gc.dim_a, proj_dim_a)
+        #self.proj_v = nn.Linear(gc.dim_v, proj_dim_v)
         self.transformer_encoder = Models.TransformerEncoder((gc.dim_l, proj_dim_a, proj_dim_v),
                                                              conf['n_layers'], conf['dropout'])
         dim_total_proj = conf['dim_total_proj']
@@ -31,7 +80,8 @@ class Net(nn.Module):
         # self.sigmoid = nn.Sigmoid()
 
     def forward(self, words, covarep, facet, inputLens):
-        state = self.transformer_encoder((words, self.proj_a(covarep), self.proj_v(facet)))
+        state = self.transformer_encoder((words, covarep, facet))
+        #state = self.transformer_encoder((words, self.proj_a(covarep), self.proj_v(facet)))
         # convert input to GRU from shape [batch_size, seq_len, input_size] to [seq_len, batch_size, input_size]
         _, gru_last_h = self.gru(state.transpose(0, 1))
         return self.finalW(gru_last_h.squeeze()).squeeze()
